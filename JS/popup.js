bb8117b8b684024af167088450945db5f57bddbb
ArturVuilov1992 <87204984+ArturVuilov1992@users.noreply.github.com>
2021-07-23 20:18:03 +0300

    hhh

--- JS/popup.js
+++ JS/popup.js
@@ -1,6 +1,13 @@
-const vector = document.querySelector(".vector")
+const popup = document.querySelector(".popup")
 const hamburger = document.querySelector(".hamburger")
+const vector = document.querySelector(".vector")
 
-popup.addEventListener("click", e => {
-       e.target.style.color = 'yellow';
-      });
+hamburger.addEventListener("click", e => {
+    e.preventDefault();
+    popup.classList.remove('popup__hidden');
+});
+
+vector.addEventListener("click", e => {
+    e.preventDefault();
+    popup.classList.add('popup__hidden');
+});

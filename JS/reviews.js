fe8bc57fd38eb94cbca8710374725b2fbfb2de14
ArturVuilov1992 <87204984+ArturVuilov1992@users.noreply.github.com>
2021-07-30 14:04:29 +0300

    еее

--- JS/reviews.js
+++ JS/reviews.js
@@ -1,15 +1,15 @@
 
 const findBlockByAlias = (alias) =>{
     return $(".reviews-block-inner").filter((ndx, item) => {
-            return $(item).attr("data-linked-with") == alias;
+            return $(item).attr("data-linked-with") == alias; //return li item which == alias
     })
 }
 $(".interactive-avatar-link").on("click", function(e){
     e.preventDefault();
-    const $this = $(e.currentTarget);
-    const target = $this.attr("data-open");
-    const itemToShow = findBlockByAlias(target);
-    const curItem = $this.closest(".interactive-avatar")
+    const $this = $(e.currentTarget);//clicked point
+    const target = $this.attr("data-open");//exact point,our interactive link with data
+    const itemToShow = findBlockByAlias(target);// li item == target == interactive link
+    const curItem = $this.closest(".interactive-avatar") //curitem == closest interactive link
 
     itemToShow.addClass("active").siblings().removeClass("active")
     curItem.addClass("active-active").siblings().removeClass("active-active");
---
+++ JS/team.js
@@ -0,0 +1,28 @@
+const openItem = item => {
+    const li = item.closest(".team__item");
+    const wrap = li.find(".team__content");
+    const text = wrap.find(".team__content-block");
+    const textHeight = text.height();
+
+    li.addClass("active");
+    wrap.height(textHeight);
+   }
+const closeEveryItem = (ul) => {
+    const wrap = ul.find(".team__content");
+   const li = ul.find(".team__item");
+
+    li.removeClass("active");
+    wrap.height(0);
+};
+
+$(".team__title").on("click", function (e){
+const $this = $(e.currentTarget);
+const ul = $this.closest(".team");
+const li = $this.closest(".team__item");
+
+if (li.hasClass("active")) {
+    closeEveryItem(ul);
+} else {
+closeEveryItem(ul);
+openItem($this);
+};})

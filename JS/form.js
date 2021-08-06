c74be376a6660860fe98be5c7f111607e3b0e8b9
ArturVuilov1992 <87204984+ArturVuilov1992@users.noreply.github.com>
2021-08-06 17:56:36 +0300

    nnnn

--- JS/form.js
+++ JS/form.js
@@ -1,18 +1,16 @@
 const form = document.querySelector('.form');
 const send = document.querySelector('.btn__form');
 const server = document.querySelector(".server");
-
-​send.addEventListener('click', (event) => {
-    event.preventDefault();
-​    if (validateForm(form)) {
+send.addEventListener('click', event => {
+event.preventDefault();
+if (validateForm(form)) {
             const data = {
                 name: form.elements.name.value,
                 phone: form.elements.phone.value,
                 comment: form.elements.comments.value,
                 to: 'test@test.com',
             };
-
-​            const xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
             xhr.responseType = "json";
             xhr.open("POST", "https://webdev-api.loftschool.com/sendmail");
             xhr.setRequestHeader('content-type', 'application/json');
@@ -24,40 +22,27 @@ const server = document.querySelector(".server");
                 console.log(xhr.response)}})
         console.log("data is sent")
           }
-
-​function validateForm(form) {
-    let valid = true;
-​
-    if (!validateField(form.elements.name)) {
-        valid = false;
-    }
-​    console.log(form.elements.phone)
-    if (!validateField(form.elements.phone)) {
-        valid = false;
-    }
+function validateForm(form) {let valid = true;if(!validateField(form.elements.name)){valid = false;}
+​console.log(form.elements.phone);if(!validateField(form.elements.phone)){valid = false;}
 ​    if (!validateField(form.elements.comments)) {
         valid = false;
     }
     return valid;
 ​}
-
 ​function validateField(field) {
 ​    if (!field.checkValidity()) {
         field.classList.add("error");
         return false;
     } 
-
 ​    else {
         field.classList.remove("error");
         return true;}
     }
 ​  })
-
 ​$(".server__link").on("click", e => {
         e.preventDefault();
         $(".server").addClass('server__hidden');
     });
-
 const openItem = item => {
     const li = item.closest(".team__item");
     const wrap = li.find(".team__content");
@@ -66,14 +51,12 @@ const openItem = item => {
     li.addClass("active");
     wrap.height(textHeight);
    }
-
 const closeEveryItem = (ul) => {
     const wrap = ul.find(".team__content");
    const li = ul.find(".team__item"); 
     li.removeClass("active");
     wrap.height(0);
 };
-
 $(".team__title").on("click", function (e){
 const $this = $(e.currentTarget);
 const ul = $this.closest(".team");
--- JS/horizontal.js
+++ JS/horizontal.js
@@ -1,68 +1,61 @@
-$(".products_menu-title").on("click", e => {
+$(".products_menu-link").on("click", e => {
 e.preventDefault();
-
 function openItem (content) {
-    const hiddenContent = content.find(".products_menu-content");
+    const hiddenContent = content.find(".products_menu-container");
     const reqWidth = mesureWidth(content);
-    const textBlock = item.find(".products_menu-container")
-
-    content.addClass("active");
+    const textBlock = item.find(".products_menu-text")
+    content.addClass(".products_menu-container-active");
     hiddenContent.width(reqWidth.container);
     textBlock.width(reqWidth.textContainer);
 };
 
+
 const mesureWidth = item => {
     let regItemWidth = 0;
-
     const screenWidth = $(window).width();
     const container = item.closest(".products_menu");
-    const titlesBlocks = container.find(".products_menu-title");
+
+
+    const titlesBlocks = container.find(".products_menu-link");
     const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
     const textContainer = item.find(".products_menu-container");
     const paddingLeft = parseInt(textContainer.css("padding-left"));
     const paddingRight = parseInt(textContainer.css("padding-right"));
-
     const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
     if (isMobile){
         regItemWidth =  screenWidth - titlesWidth;
     } else {
         regItemWidth =  500;
     }
-
     return {
         container: regItemWidth,
         textContainer: regItemWidth - paddingRight - paddingLeft
     }
 };
-
-
 function closeEveryItemInContainer (container) {
     const items = container.find(".products_menu-item");
-    const content = container.find(".products_menu-content");
+    const content = container.find(".products_menu-container");
     items.removeClass("active");
     content.width(0);
 };
-
 const $this = $(e.currentTarget);
 const item = $this.closest(".products_menu-item");
-const itemOpened = item.hasClass("active");
+const itemOpened = item.hasClass(".products_menu-container-active");
 const container = $this.closest(".products_menu")
-
 if (itemOpened) {
     closeEveryItemInContainer(container)
 } else {
     closeEveryItemInContainer(container)
-
     openItem(item);
 }
+
+
+
 
 });
 
 
-
-
-$(".products_menu-close").on("click", e =>{
+$(".products_menu-item").on("click", e =>{
     e.preventDefault();
 
     closeEveryItemInContainer(".products_menu");
